db9a3bb0b793042e8e428e62ea1e41869469d0c2
TerryCaporicci <terry.caporicci@epitech.eu>
2019-05-24 17:27:14 +0200

    adding of the components

---
+++ bot-battlefield-app/src/app/home/opponent-selection/opponent-selection.component.js
@@ -0,0 +1,9 @@
+import {PlayerListSelection} from "./player-list/player-list.component";
+
+export class OpponentSelection {
+    constructor () {
+        super();
+
+        playerListSelection = new PlayerListSelection();
+    }
+}
---
+++ bot-battlefield-app/src/app/home/opponent-selection/player-list/player-list.component.js
@@ -0,0 +1,5 @@
+export class PlayerListSelection {
+    constructor () {
+        super();
+    }
+}
